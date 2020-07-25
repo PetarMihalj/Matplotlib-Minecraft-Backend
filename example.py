dc87b89a39a5a2ab21ad051654cffbdfffc21200
Petar Mihalj <petar.mihalj.nasice@gmail.com>
2020-07-25 16:08:04 +0200

    packed up, should be functional

--- example.py
+++ example.py
@@ -1,7 +1,9 @@
+from mc_mpl.mde import MDE
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.use("module://mc_mpl.mc_backend")
 
+MDE().mc.player.setPos(0, 10, 0)
 
 plt.plot([1, 2, 1])
 plt.plot([1, 2, 1])
--- run_on_file.sh
+++
@@ -1,3 +0,0 @@
-rm -rf ./server/world
-cd server
-java -jar craftbukkit-1.12.1.jar 
